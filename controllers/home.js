b01e79906a62855da7a13908c282bd105fec823a
Lizzie Koehler <lizziefkoehler@gmail.com>
2015-03-02 15:18:44 -0800

    update carousel to move both forward and backwards and loop

--- controllers/home.js
+++ controllers/home.js
@@ -4,7 +4,20 @@ Bustle.HomeController = Ember.Controller.extend({
     switchPicture: function() {
       var imageArray = ["css/img/benjaminsbooks.jpg", "css/img/food.jpg", "css/img/movies.jpg", "css/img/music.jpg"];
       var i = imageArray.indexOf(this.get("whichPicture"));
-      this.set('whichPicture', imageArray[i+1]);
+      if ( i<3) {
+        this.set('whichPicture', imageArray[i+1]);
+      } else {
+        this.set('whichPicture', "css/img/benjaminsbooks.jpg")
+      }
+    },
+    backSwitchPicture: function() {
+      var imageArray = ["css/img/benjaminsbooks.jpg", "css/img/food.jpg", "css/img/movies.jpg", "css/img/music.jpg"];
+      var i = imageArray.indexOf(this.get("whichPicture"));
+      if ( i>0) {
+        this.set('whichPicture', imageArray[i-1]);
+      } else {
+        this.set('whichPicture', "css/img/music.jpg")
+      }
     }
   }
 });
